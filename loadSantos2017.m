da6f4b40aa06e2aa18f34e31646be2e6b58793dd
Matto <64538595+mleeuwis@users.noreply.github.com>
2023-05-25 11:18:39 +0200

    Rename and reorder markers to Rajagopal model

--- loadSantos2017.m
+++ loadSantos2017.m
@@ -39,29 +39,31 @@ for i = 1:size(Measurements,1)
 end
 
 %% Marker names
-mkr_names = readtable("mkr_names.xlsx", 'VariableNamingRule', 'preserve')
+mkr_names = readtable("mkr_names.xlsx", 'VariableNamingRule', 'preserve');
 
 %% Save renamed marker data to the selected measurements
 Measurements.mkr_Rajagopal = cell(size(Measurements.mkr));
 for i = 1:size(Measurements,1)
-    T_marker = table();
-    T_marker.("Frame#") = (1:size(Measurements_selected.T_mocap{i},1))';
-    T_marker.Time = Measurements_selected.T_mocap{i}.TimeStamp;
-    T_marker.Time = linspace(min(T_marker.Time), max(T_marker.Time), length(T_marker.Time))'; %some measurements contain skipped measuremnts, normalize time here
-    for j = 1:size(MarkerNamesHuawei2020_matched,1)
-        T_mocap = Measurements_selected.T_mocap{i};
-        markerHuawei = MarkerNamesHuawei2020_matched(j,1);
-        markerRajagopal = MarkerNamesHuawei2020_matched(j,2);
-
-        if markerRajagopal ~= 'false'
-            X = T_mocap.(markerHuawei + "_X");
-            Y = T_mocap.(markerHuawei + "_Y");
-            Z = T_mocap.(markerHuawei + "_Z");
-
-            T_marker.(markerRajagopal) = 1000*[-X,Y,-Z];
+    if Measurements.bool_read(i) == 1 && Measurements.bool_transform(i) == 1
+        T_marker = table();
+        T_marker.("Frame#") = (1:size(Measurements.mkr{i},1))';
+        T_marker.Time = Measurements.mkr{i}.Time;
+        T_marker.Time = linspace(min(T_marker.Time), max(T_marker.Time), length(T_marker.Time))'; %some measurements contain skipped measuremnts, normalize time here
+        for j = 1:size(mkr_names,1)
+            mkr_transformed = Measurements.mkr{i};
+            markerSantos = string(mkr_names{j,2});
+            markerRajagopal = string(mkr_names{j,1});
+    
+            if (markerRajagopal ~= "") && (markerSantos ~= "")
+                X = mkr_transformed.(markerSantos + "_X");
+                Y = mkr_transformed.(markerSantos + "_Y");
+                Z = mkr_transformed.(markerSantos + "_Z");
+    
+                T_marker.(markerRajagopal) = 1000*[-X,Y,-Z];
+            end
         end
+        Measurements.mkr_Rajagopal{i} = T_marker;
     end
-    Measurements_selected.T_marker{i} = T_marker;
 end
 
 
